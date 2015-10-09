b0f03cf5c74fc4003e46ee9c2c64e63fbb661cde
cts466846 <subin.v@cognizant.com>
2015-10-09 17:27:21 +0530

    initialCommit

---
+++ SampleOfGarbageCollection/GCCollectClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SampleOfGarbageCollection
+{
+	public class GCCollectClass
+	{
+		public GCCollectClass ()
+		{
+		}
+	}
+	public class ParentClass
+	{
+		public string theString;
+		public string getString(string s)
+		{
+			theString = s;
+			return theString;
+		}
+	}
+	class ChildClass : ParentClass
+	{
+		public int theInt = 0;
+		public int[] theIntArray = new int[10000];
+
+		public int getInt(int i)
+		{
+			theInt = i;
+			return theInt;
+		}
+
+
+	}
+}
+
---
+++ SampleOfGarbageCollection/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SampleOfGarbageCollection
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			ParentClass p = new ParentClass();
+			string s = p.getString ("Hello");
+			Console.WriteLine ("String "+s+".");
+			ChildClass c = new ChildClass ();
+			int a = c.getInt (10);
+			Console.WriteLine ("Integer " + a + ".");
+			MainClass m = new MainClass ();
+			m.GarbageMethod (2000);
+			Console.WriteLine ("Before calling GC.Collect() "+ GC.GetTotalMemory(false));
+			GC.Collect ();
+			Console.WriteLine ("After calling GC.Collect() "+ GC.GetTotalMemory(false));
+		}
+		public void GarbageMethod(int value)
+		{
+			ChildClass ch;
+			for (int i=0; i < value ; i++)
+			{
+				ch = new ChildClass ();
+			}
+		}
+	}
+}
